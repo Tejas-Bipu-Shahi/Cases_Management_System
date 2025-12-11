b5883d975877dfb61aff869d956e9bd92988916b
Tejas-Bipu-Shahi <shahitejas899@gmail.com>
2025-12-11 12:23:21 +0545

    Initial Commit - connect with github

---
+++ src/cases_management_system/cms/Cases_Management_SystemCMS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cases_management_system.cms;
+
+/**
+ *
+ * @author tejas
+ */
+public class Cases_Management_SystemCMS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
